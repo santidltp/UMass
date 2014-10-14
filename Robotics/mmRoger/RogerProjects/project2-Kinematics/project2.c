77e732d6a3333817b7084343fa0eb2d3019047cf
Santiago <sdelatorrepi@umass.edu>
2014-10-14 14:00:33 -0400

    arms dnearly done in roger

--- Robotics/mmRoger/RogerProjects/project2-Kinematics/project2.c
+++ Robotics/mmRoger/RogerProjects/project2-Kinematics/project2.c
@@ -34,39 +34,39 @@ double x, y;
 
   // transform world frame (x,y)_w input into base coordinates (x,y)_b
   // This piece of code has been extracted from porject1.c
-    double s1, c1;
-    double base_pos[3];
-  s1 = sin(base_pos[2]);
-  c1 = cos(base_pos[2]);
 
-
-  wTb[0][0] = c1;  wTb[0][1] = -s1; wTb[0][2] = 0.0; wTb[0][3] = base_pos[X];
-  wTb[1][0] = s1;  wTb[1][1] = c1;  wTb[1][2] = 0.0; wTb[1][3] = base_pos[Y];
-  wTb[2][0] = 0.0; wTb[2][1] = 0.0; wTb[2][2] = 1.0; wTb[2][3] = 0.0;
-  wTb[3][0] = 0.0; wTb[3][1] = 0.0; wTb[3][2] = 0.0; wTb[3][3] = 1.0;
+  ref_w[X]=x;
+  ref_w[Y]=y;
+  ref_w[2]=0;
+  ref_w[3]=1;
+  construct_wTb(roger->base_position,wTb);
+  inverse_homogeneous_xform(wTb,bTw);
+  matrix_times_vector(bTw,ref_w,ref_b);
 
   // add the arm offset for the approariate arm to get it to shoulder frame
   if (limb==LEFT) ref_b[Y] -= ARM_OFFSET;
-  else ref_b[Y] += ARM_OFFSET;//??
+  else ref_b[Y] += ARM_OFFSET;
 
   // implement the inverse kinematic algorithm
   //my algorithm
-  r2=(x*x)+(y*y);//or base_pos[pos]?
+  r2=(ref_b[X]*ref_b[X])+(ref_b[Y]*ref_b[Y]);//or base_pos[pos]?
   c2=((r2-(LARM_1*LARM_1)-(LARM_2*LARM_2)))/(2*LARM_1*LARM_2);
-  if(-1<=c2<=1){
-    s2_plus        = pow((1-pow(c2,2)),0.5);
-    s2_minus       = -pow((1-pow(c2,2)),0.5);
-    theta2_plus    = atan(s2_plus/c2);
-    theta2_minus   = atan(s2_minus/c2);
-    k1             = LARM_1+(LARM_2*c2);
-    k2_plus        = LARM_2*s2_plus;
-    k2_minus       = LARM_2*s2_minus;
-    alpha_plus     = atan(k2_plus/k1);
-    alpha_minus    = atan(k2_minus/k1);
-    theta1_minus   = atan(y/x)-alpha_minus ;
-    theta1_plus    = atan(y/x)-alpha_plus;
-  //  roger->arm_setpoint[LEFT][(int)theta1_plus];//??
 
+
+  if(-1<=c2 && c2<=1){
+    s2_plus                       = pow((1-pow(c2,2)),0.5);
+    s2_minus                      = -pow((1-pow(c2,2)),0.5);
+    theta2_plus                   = atan2(s2_plus,c2);
+    theta2_minus                  = atan2(s2_minus, c2);
+    k1                            = LARM_1+(LARM_2*c2);
+    k2_plus                       = LARM_2*s2_plus;
+  k2_minus                        = LARM_2*s2_minus;
+    alpha_plus                    = atan2(k2_plus,k1);
+    alpha_minus                   = atan2(k2_minus,k1);
+    theta1_minus                  = atan2(y,x)-alpha_minus ;
+    theta1_plus                   = atan2(y,x)-alpha_plus;
+    roger->arm_setpoint[limb][0]  = theta1_plus;
+    roger->arm_setpoint[limb][1]  = theta2_plus;
     return (TRUE); //solution
   }
   else
